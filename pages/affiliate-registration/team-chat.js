3c96f4e288f90332ef4e0f2c7d662f936c5772a9
Tipu <30400782+tipu07@users.noreply.github.com>
2023-09-04 15:46:56 +0500

    Affiliate Registration

---
+++ pages/affiliate-registration/team-chat.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react"
+import style from "@/styles/scss/web.module.scss"
+import AffiliateRegistrationHeader from "@/components/affiliateRegistrationHeader"
+import AffiliateRegistrationSidebar from "@/components/affiliateRegistrationSidebar"
+import MessagesDetail from "./messages/messagesDetail"
+
+export default function TeamChat() {
+	const [toggle, setToggle] = useState(false)
+	const toggleHandle = () => {
+		setToggle(!toggle)
+	}
+	return (
+		<>
+			<section className={`${style.dashboard} ${toggle ? style.flow : ""}`} id={style.message}>
+				<AffiliateRegistrationSidebar isActive={toggle} />
+				<div id={style.main_area}>
+					<AffiliateRegistrationHeader isActive={toggle} onToggle={toggleHandle} pageTitle="Team Chat" />
+					<MessagesDetail />
+				</div>
+			</section>
+		</>
+	)
+}
---
+++ pages/affiliate-registration/withdrawRequest/withdrawRequestDetail.js
@@ -0,0 +1,25 @@
+import React from "react"
+import style from "@/styles/scss/web.module.scss"
+import WithdrawRequestBlocks from "./withdrawRequestBlocks"
+import WithdrawRequestForm from "./withdrawRequestForm"
+
+export default function WithdrawRequestDetail() {
+	return (
+		<>
+			<div className="row">
+				<div className="col-lg-9">
+					<WithdrawRequestBlocks />
+					<div className="py-4"></div>
+					<form action="" method="post" className={style.application_form}>
+						<WithdrawRequestForm />
+						<div className={`${style.btn_blk} justify-content-end mt-5`}>
+							<button type="button" className={style.site_btn}>
+								Send Request
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+	)
+}
